4a0bf04234e2719c3c7715b5f70818688c16e4b3
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-06-26 08:10:10 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,11 @@
 @echo off
 python -m pip install virtualenv
-call install.bat ::После установки всех пакетов можете удалить эту строку, чтобы пропустить проверку. 
+
+call install.bat
+REM После установки всех пакетов можете удалить эту строку, чтобы пропустить проверку. 
 
 call venv\Scripts\activate.bat
 python run.py
 pause
 
-:: Упаковано и собрано телеграм каналом Neutogen News: https://t.me/neurogen_news
+REM Упаковано и собрано телеграм каналом Neutogen News: https://t.me/neurogen_news
