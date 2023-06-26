cac141da575c4d641b874971b401bd3f43198b3c
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-06-26 11:58:17 +0800

    bat files for easy launch

--- start.bat
+++ start.bat
@@ -1,11 +1,5 @@
 @echo off
-python -m pip install virtualenv
-
-call install.bat
-REM После установки всех пакетов можете удалить эту строку, чтобы пропустить проверку. 
 
 call venv\Scripts\activate.bat
 python run.py
 pause
-
-REM Упаковано и собрано телеграм каналом Neutogen News: https://t.me/neurogen_news
