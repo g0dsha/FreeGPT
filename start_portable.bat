437e669cf6b105c921a3d4e4bf2cf45ea6f8f51b
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-06-28 13:39:42 +0800

    Add files via upload

---
+++ start_portable.bat
@@ -0,0 +1,19 @@
+@echo off
+set pypath=home = %~dp0python
+set venvpath=_ENV=%~dp0venv
+if exist venv (powershell -command "$text = (gc venv\pyvenv.cfg) -replace 'home = .*', $env:pypath; $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False);[System.IO.File]::WriteAllLines('venv\pyvenv.cfg', $text, $Utf8NoBomEncoding);$text = (gc venv\scripts\activate.bat) -replace '_ENV=.*', $env:venvpath; $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False);[System.IO.File]::WriteAllLines('venv\scripts\activate.bat', $text, $Utf8NoBomEncoding);")
+
+for /d %%i in (tmp\tmp*,tmp\pip*) do rd /s /q "%%i" 2>nul || ("%%i" && exit /b 1) & del /q tmp\tmp* > nul 2>&1 & rd /s /q pip\cache 2>nul
+
+set appdata=tmp
+set userprofile=tmp
+set temp=tmp
+set PATH=git\cmd;python;venv\scripts;
+
+call venv\Scripts\activate.bat
+python run.py --web_results 5 --max_conversation_length 12
+pause
+
+REM Упаковано и собрано телеграм каналом Neutogen News: https://t.me/neurogen_news
+
+
