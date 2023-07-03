8be8294c97d729d9f257a6dc55d0f36d5506655c
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-07-03 16:36:51 +0800

    Update models.py

--- g4f/models.py
+++ g4f/models.py
@@ -12,7 +12,7 @@ class Model:
         name: str = 'gpt-3.5-turbo'
         base_provider: str = 'openai'
         #best_provider: Provider.Provider = Provider.DeepAi
-        best_provider: Provider.Provider = random.choice([Provider.DeepAi, Provider.Fakeopen])
+        best_provider: Provider.Provider = random.choice([Provider.DeepAi, Provider.Gravityengine])
         best_providers: list = [Provider.Forefront, Provider.Lockchat, Provider.Yqcloud]
 
     class gpt_35_turbo_0613:
